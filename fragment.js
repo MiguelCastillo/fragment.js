f71a9a14f2a665f0f0be05292a5ab2dc723bfaba
Miguel Castillo <manchagnu@gmail.com>
2013-12-14 19:28:54 -0500

    Initial source commit

---
+++ fragment.js
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2013 Miguel Castillo.
+ * Licensed under MIT
+ *
+ * https://github.com/MiguelCastillo/fragment.js
+ */
+
+
+(function(factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(["resource"], factory);
+  } else {
+    // Browser globals
+    this.$.fragment = factory(this.$.resource);
+  }
+})
+(function( resource ) {
+  "use strict";
+
+
+  function fragment(settings, selector) {
+    settings = settings || {};
+    selector = selector || fragment.selector;
+
+    var deferred = $.Deferred(),
+        attrSelector = "[" + selector + "]";
+
+    if (typeof settings.url === "string" ) {
+      fragment.loader(settings)
+        .done(deferred.resolve)
+        .fail(deferred.reject);
+    }
+    else if (typeof settings.html === "string"
+             || settings.html instanceof jQuery === true ) {
+      deferred.resolve(settings.html);
+    }
+    else {
+      deferred.resolve(settings);
+    }
+
+    // Handle nested fragment loading
+    return deferred.then(function(tmpl) {
+      var $tmpl = $(tmpl);
+
+      var done = $tmpl.filter(attrSelector)
+        .add($tmpl.children(attrSelector))
+        .add($tmpl.find(attrSelector))
+        .map(function() {
+          var $this = $(this);
+          return fragment({
+              "url": $this.attr(selector)
+            })
+            .done(function(_tmpl){
+              $this.append($(_tmpl));
+            });
+        });
+
+      return $.when.apply($, done).then(function() {
+        return $tmpl;
+      });
+    });
+  }
+
+
+  fragment.selector = "mjs-fragment";
+  fragment.loader = resource;
+  return fragment;
+});
